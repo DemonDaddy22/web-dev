e1097a2fb38526b3acf3caed5c4b03eb3106b860
Rohan Gupta <guptarohan369@gmail.com>
2020-12-13 19:16:14 +0530

    make API call to fetch shows info for entered query

--- TVShowSearch/index.js
+++ TVShowSearch/index.js
@@ -1,8 +1,43 @@
 const form = document.querySelector('#searchForm');
+const shows = document.querySelector('.shows');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', async e => {
     e.preventDefault();
-    
-    const query = form.elements.query.value;
-    console.log(query);
+
+    const query = form.elements.search.value;
+    const fetchedShows = await fetchShows(query) || [];
+    console.log(fetchedShows);
+
+    shows.innerHTML = '';
+    fetchedShows.forEach(show => {
+        // TODO - getShowContent for each show
+        shows.append(getShowContent(show));
+    });
 });
+
+const fetchShows = async query => {
+    if (isEmptyString(query)) return null;
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+    return res.data;
+}
+
+const getShowContent = show => {
+    const showContainer = document.createElement('div');
+    showContainer.classList.add('showContainer');
+
+    // TODO - add column for image
+    const col1 = document.createElement('div');
+    col1.classList.add('col-1');
+    const image = document.createElement('img');
+    image.src = show.show.image.original || '';
+    image.alt = show.show.name || 'show-posters';
+    image.classList.add('poster');
+    col1.append(image);
+    showContainer.append(col1);
+
+    // TODO - add column for content
+
+    return showContainer;
+}
+
+const isEmptyString = data => typeof data !== 'string' || !data || data.length === 0;
