44d12afcd1aadae8f67d5b2d3394a6af84dc4590
Rohan Gupta <guptarohan369@gmail.com>
2020-12-13 21:06:07 +0530

    create tv shows container

--- TVShowSearch/index.js
+++ TVShowSearch/index.js
@@ -28,14 +28,50 @@ const getShowContent = show => {
     // TODO - add column for image
     const col1 = document.createElement('div');
     col1.classList.add('col-1');
+    
     const image = document.createElement('img');
     image.src = show.show.image.original || '';
     image.alt = show.show.name || 'show-posters';
     image.classList.add('poster');
+
     col1.append(image);
     showContainer.append(col1);
 
     // TODO - add column for content
+    const col2 = document.createElement('div');
+    col2.classList.add('col-2');
+    
+    const h2 = document.createElement('h2');
+    h2.classList.add('showTitle');
+    h2.innerText = show.show.name || '';
+
+    const genres = document.createElement('div');
+    genres.classList.add('genres');
+    let genresText = '';
+    if (show.show.genres) show.show.genres.forEach((genre, index) => {
+        genresText += genre;
+        if (index !== show.show.genres.length - 1) genresText += ', ';
+    });
+    genres.innerText = genresText;
+
+    const ratings = document.createElement('div');
+    ratings.classList.add('rating');
+    ratings.innerText = show.show.rating.average ? `${show.show.rating.average} ⭐` : '';
+
+    const premiered = document.createElement('div');
+    premiered.classList.add('premiered');
+    premiered.innerHTML = show.show.premiered || '';
+
+    const description = document.createElement('div');
+    description.classList.add('description');
+    description.innerHTML = show.show.summary || '';
+
+    col2.append(h2);
+    col2.append(genres);
+    col2.append(ratings);
+    col2.append(premiered);
+    col2.append(description);
+    showContainer.append(col2);
 
     return showContainer;
 }
