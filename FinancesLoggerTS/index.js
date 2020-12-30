79870072631a95f9bff759d5858b7bce2109b0a6
Rohan Gupta <guptarohan369@gmail.com>
2020-12-30 19:13:11 +0530

    add access modifiers to Invoice variables

--- FinancesLoggerTS/index.js
+++ FinancesLoggerTS/index.js
@@ -1,7 +1,31 @@
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
+var Invoice = /** @class */ (function () {
+    // private client: string;
+    // private details: string;
+    // private amount: number;
+    // constructor(client: string, details: string, amount: number) {
+    //     this.client = client;
+    //     this.details = details;
+    //     this.amount = amount;
+    // }
+    // works only when we have specified access modifiers
+    function Invoice(client, details, amount) {
+        var _this = this;
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+        this.format = function () { return _this.client + " owes \u00A3" + _this.amount + " for " + _this.details; };
+    }
+    return Invoice;
+}());
+var invoiceOne = new Invoice('Mario', 'Work on Mario website', 250);
+var invoiceTwo = new Invoice('Luigi', 'Work on Luigi website', 300);
+var invoices = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+invoices.forEach(function (invoice) { return console.log(invoice.format()); });
 var form = document.querySelector('.new-item-form');
-// console.log(form.children);
 var type = document.querySelector('#type');
 var toFrom = document.querySelector('#toFrom');
 var details = document.querySelector('#details');
--- FinancesLoggerTS/index.ts
+++ FinancesLoggerTS/index.ts
@@ -1,8 +1,36 @@
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
 
+class Invoice {
+    // private client: string;
+    // private details: string;
+    // private amount: number;
+
+    // constructor(client: string, details: string, amount: number) {
+    //     this.client = client;
+    //     this.details = details;
+    //     this.amount = amount;
+    // }
+
+    // works only when we have specified access modifiers
+    constructor(
+        readonly client: string,
+        private details: string,
+        readonly amount: number
+    ) { }
+
+    format = () => `${this.client} owes £${this.amount} for ${this.details}`;
+}
+
+const invoiceOne = new Invoice('Mario', 'Work on Mario website', 250);
+const invoiceTwo = new Invoice('Luigi', 'Work on Luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+invoices.forEach(invoice => console.log(invoice.format()));
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
