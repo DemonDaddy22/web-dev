9f75d334bf142254453cd02d8f16ccdf7bf70d55
Rohan Gupta <guptarohan369@gmail.com>
2021-01-15 22:29:30 +0530

    add post and put endpoints, and add readFile function to read from data.json

--- PostsREST/dist/index.dev.js
+++ PostsREST/dist/index.dev.js
@@ -2,13 +2,36 @@
 
 var express = require('express');
 
-var bodyParser = require('body-parser');
-
-var data = require('./data.json');
+var fs = require('fs');
 
 var app = express();
+app.use(express.urlencoded({
+  extended: true
+}));
+
+var readFile = function readFile(file) {
+  return fs.readFileSync(__dirname + '/' + file, {
+    encoding: 'utf8'
+  });
+};
+
 app.get('/posts', function (req, res) {
-  res.status(200).send(data);
+  var posts = readFile('data.json');
+  res.status(200).send(JSON.parse(posts));
+});
+app.post('/posts', function (req, res) {
+  var content = req.body.content;
+  var newPost = {
+    id: data.data[data.data.length - 1].id + 1,
+    content: content,
+    upvotes: 0,
+    downvotes: 0,
+    comments: []
+  };
+  data.data.push(newPost);
+  res.status(200).send({
+    data: newPost
+  });
 });
 app.get('/posts/:id', function (req, res) {
   var id = req.params.id;
@@ -20,6 +43,33 @@ app.get('/posts/:id', function (req, res) {
   });
   res.status(200).send({
     data: [post[0]]
+  });
+});
+app.put('/posts/:id', function (req, res) {
+  var id = req.params.id;
+  var _req$body = req.body,
+      content = _req$body.content,
+      upvotes = _req$body.upvotes,
+      downvotes = _req$body.downvotes,
+      comments = _req$body.comments;
+  var post = data.data.filter(function (post) {
+    return post.id === parseInt(id);
+  });
+  if (!post.length) res.status(404).send({
+    data: "No post found for id: ".concat(id)
+  });
+  var updatedPost = {
+    id: post.id,
+    content: content,
+    upvotes: upvotes,
+    downvotes: downvotes,
+    comments: comments
+  };
+  data.data = data.data.map(function (post) {
+    return post.id === parseInt(id) ? updatedPost : post;
+  });
+  res.status(200).send({
+    data: updatedPost
   });
 });
 app.listen(8080, function (req, res) {
--- PostsREST/index.js
+++ PostsREST/index.js
@@ -1,16 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const data = require('./data.json');
+const fs = require('fs');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const readFile = file => fs.readFileSync(__dirname + '/' + file, { encoding: 'utf8' });
 
 app.get('/posts', (req, res) => {
-    res.status(200).send(data);
+    let posts = readFile('data.json');
+    res.status(200).send(JSON.parse(posts));
+});
+
+app.post('/posts', (req, res) => {
+    const { content } = req.body;
+
+    const newPost = {
+        id: data.data[data.data.length - 1].id + 1,
+        content,
+        upvotes: 0,
+        downvotes: 0,
+        comments: []
+    };
+
+    data.data.push(newPost);
+
+    res.status(200).send({ data: newPost });
 });
 
 app.get('/posts/:id', (req, res) => {
     const { id } = req.params;
-    
+
     const post = data.data.filter(post => post.id === parseInt(id));
 
     if (!post.length) res.status(200).send({
@@ -19,6 +38,25 @@ app.get('/posts/:id', (req, res) => {
 
     res.status(200).send({
         data: [post[0]]
+    });
+});
+
+app.put('/posts/:id', (req, res) => {
+    const { id } = req.params;
+
+    const { content, upvotes, downvotes, comments } = req.body;
+    const post = data.data.filter(post => post.id === parseInt(id));
+
+    if (!post.length) res.status(404).send({
+        data: `No post found for id: ${id}`
+    });
+
+    const updatedPost = { id: post.id, content, upvotes, downvotes, comments };
+
+    data.data = data.data.map(post => post.id === parseInt(id) ? updatedPost : post);
+
+    res.status(200).send({
+        data: updatedPost
     });
 });
 
