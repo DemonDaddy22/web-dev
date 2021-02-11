e21c6293c7451953c1f727131c2498dd0680a177
Rohan Gupta <guptarohan369@gmail.com>
2021-02-12 00:36:42 +0530

    Wrap navbar component with AuthContext.Consumer

--- hooks-context-practice/src/components/Navbar.js
+++ hooks-context-practice/src/components/Navbar.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 export default class Navbar extends React.Component {
 
     // static contextType = ThemeContext;
 
-    render = () => <ThemeContext.Consumer>
-        {context => {
-            const { isLightTheme, light, dark } = context;
-            const theme = isLightTheme ? light : dark;
-            
-            return <nav style={{ background: theme.ui, color: theme.syntax }}>
-                <h2>Hooks & Context</h2>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        }}
-    </ThemeContext.Consumer>;
+    render = () => <AuthContext.Consumer>
+        {authContext => <ThemeContext.Consumer>
+            {themeContext => {
+                const { isAuthenticated, toggleAuth } = authContext;
+                const { isLightTheme, light, dark } = themeContext;
+                const theme = isLightTheme ? light : dark;
+
+                return <nav style={{ background: theme.ui, color: theme.syntax }}>
+                    <h2>Hooks & Context</h2>
+                    <p onClick={toggleAuth}>{isAuthenticated ? 'Signed in' : 'Signed out'}</p>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            }}
+        </ThemeContext.Consumer>}
+    </AuthContext.Consumer>;
 }
