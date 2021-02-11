ca60c43c86937815f2cfe188561e22256b1a428f
Rohan Gupta <guptarohan369@gmail.com>
2021-02-11 22:58:16 +0530

    Create ThemeContextProvider

--- hooks-context-practice/src/App.js
+++ hooks-context-practice/src/App.js
@@ -1,9 +1,12 @@
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 const App = () => <div className='App'>
-    <Navbar />
-    <BookList />
+    <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+    </ThemeContextProvider>
 </div>;
 
 export default App;
---
+++ hooks-context-practice/src/contexts/ThemeContext.js
@@ -0,0 +1,20 @@
+import React, { createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+export default class ThemeContextProvider extends React.Component {
+
+    state = {
+        isLightTheme: true,
+        light: {
+            syntax: '#555', ui: '#ddd', bg: '#eee'
+        },
+        dark: {
+            syntax: '#ddd', ui: '#333', bg: '#555'
+        }
+    }
+
+    render = () => <ThemeContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+    </ThemeContext.Provider>
+}
