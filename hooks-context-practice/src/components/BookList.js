fdd34603708f03f5971a8265a60b760a97a95d94
Rohan Gupta <guptarohan369@gmail.com>
2021-02-13 15:17:23 +0530

    Convert booklist into a functional component

--- hooks-context-practice/src/components/BookList.js
+++ hooks-context-practice/src/components/BookList.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-export default class BookList extends React.Component {
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
 
-    // static contextType = ThemeContext;
-
-    render = () => <ThemeContext.Consumer>
-        {context => {
-            const { isLightTheme, light, dark } = context;
-            const theme = isLightTheme ? light : dark;
-
-            return <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
-                <ul>
-                    <li style={{ background: theme.ui }}>The Way of Kings</li>
-                    <li style={{ background: theme.ui }}>The Name of the Wind</li>
-                    <li style={{ background: theme.ui }}>The Final Empire</li>
-                </ul>
-            </div>;
-        }}
-    </ThemeContext.Consumer>;
+    return <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
+        <ul>
+            <li style={{ background: theme.ui }}>The Way of Kings</li>
+            <li style={{ background: theme.ui }}>The Name of the Wind</li>
+            <li style={{ background: theme.ui }}>The Final Empire</li>
+        </ul>
+    </div>;
 }
+
+export default BookList;
+
+// export default class BookList extends React.Component {
+
+//     // static contextType = ThemeContext;
+
+//     render = () => <ThemeContext.Consumer>
+//         {context => {
+//             const { isLightTheme, light, dark } = context;
+//             const theme = isLightTheme ? light : dark;
+
+//             return <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
+//                 <ul>
+//                     <li style={{ background: theme.ui }}>The Way of Kings</li>
+//                     <li style={{ background: theme.ui }}>The Name of the Wind</li>
+//                     <li style={{ background: theme.ui }}>The Final Empire</li>
+//                 </ul>
+//             </div>;
+//         }}
+//     </ThemeContext.Consumer>;
+// }
