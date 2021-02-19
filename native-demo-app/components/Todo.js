23277c2fc968f3ba5f26cb2587c65600c6a959de
Rohan Gupta <guptarohan369@gmail.com>
2021-02-19 22:19:20 +0530

    Add material icons

--- native-demo-app/components/Todo.js
+++ native-demo-app/components/Todo.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Todo = React.memo(({ todo, onPress }) => <TouchableOpacity style={styles.todo} onPress={() => onPress(todo.id)}>
     <Text style={styles.label}>{todo.label}</Text>
+    <MaterialIcons name='delete' size={20} color='#143a59cf' />
 </TouchableOpacity>);
 
 const styles = StyleSheet.create({
@@ -15,8 +17,10 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         marginTop: 16,
         marginHorizontal: 24,
+        flexDirection: 'row'
     },
     label: {
+        flex: 1,
         color: '#143a59',
         fontSize: 20,
         fontWeight: '500'
