89dc219b34f905f9b0da25bab4127caf39b7a6f9
Rohan Gupta <guptarohan369@gmail.com>
2021-02-23 23:44:16 +0530

    Make drawer menu button work

--- native-reviews-app/AboutNavigator.js
+++ native-reviews-app/AboutNavigator.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 import About from './screens/About';
 
 const { Navigator, Screen } = createStackNavigator();
 
-const AboutNavigator = () => <NavigationContainer independent>
-    <Navigator headerMode='screen' screenOptions={{ headerStyle: { backgroundColor: '#d91d545f' } }}>
-        <Screen name='About' component={About} options={{ title: 'About GameZone' }} />
-    </Navigator>
-</NavigationContainer>;
+const AboutNavigator = ({ navigation }) => {
+
+    const openMenu = () => navigation.openDrawer();
+
+    return <NavigationContainer independent>
+        <Navigator headerMode='screen' screenOptions={{ headerStyle: { backgroundColor: '#d91d545f' } }}>
+            <Screen name='About' component={About} options={{
+                title: 'GameZone',
+                headerLeft: () => (
+                    <MaterialIcons name="menu" size={28} color='black' onPress={openMenu} />
+                ),
+                headerLeftContainerStyle: {
+                    paddingHorizontal: 20,
+                }
+            }} />
+        </Navigator>
+    </NavigationContainer>;
+}
 
 export default AboutNavigator;
--- native-reviews-app/AppNavigator.js
+++ native-reviews-app/AppNavigator.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialIcons } from '@expo/vector-icons';
 import Home from './screens/Home';
 import ReviewDetails from './screens/ReviewDetails';
 
 const { Navigator, Screen } = createStackNavigator();
 
-const AppNavigator = () => <NavigationContainer initialRouteName='Home' independent>
-    <Navigator headerMode='screen' screenOptions={{ headerStyle: { backgroundColor: '#d91d545f' } }}>
-        <Screen name='Home' component={Home} options={{ title: 'Reviews' }} />
-        <Screen name='ReviewDetails' component={ReviewDetails} options={{ title: 'Details' }} />
-    </Navigator>
-</NavigationContainer>;
+const AppNavigator = ({ navigation }) => {
+
+    const openMenu = () => navigation.openDrawer();
+
+    return <NavigationContainer initialRouteName='Home' independent>
+        <Navigator headerMode='screen' screenOptions={{ headerStyle: { backgroundColor: '#d91d545f' } }}>
+            <Screen name='Home' component={Home} options={{
+                title: 'Reviews',
+                headerLeft: () => (
+                    <MaterialIcons name='menu' size={28} color='black' onPress={openMenu} />
+                ),
+                headerLeftContainerStyle: {
+                    paddingHorizontal: 20,
+                }
+            }} />
+            <Screen name='ReviewDetails' component={ReviewDetails} options={{ title: 'Details' }} />
+        </Navigator>
+    </NavigationContainer>;
+}
 
 export default AppNavigator;
