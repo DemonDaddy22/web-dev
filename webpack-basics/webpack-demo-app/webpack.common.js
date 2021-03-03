3d056811334898b123485bcac5b7bdb00ea27260
Rohan Gupta <guptarohan369@gmail.com>
2021-03-03 23:50:45 +0530

    Split into dev and prod configs

--- webpack-basics/webpack-demo-app/webpack.common.js
+++ webpack-basics/webpack-demo-app/webpack.common.js
@@ -2,13 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
     // devtool: "source-map",
     entry: "./src/index.js",
-    output: {
-        filename: 'main.[contenthash].js',
-        path: path.resolve(__dirname, "dist")
-    },
     plugins: [new HtmlWebpackPlugin({
         template: './src/template.html'
     })],
