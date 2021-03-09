b10d8da0d6543bf60a59aee9533a632be22a8c67
Rohan Gupta <guptarohan369@gmail.com>
2021-03-09 23:55:46 +0530

    Add new product form

--- MongooseAndExpress/index.js
+++ MongooseAndExpress/index.js
@@ -7,6 +7,7 @@ const Product = require('./models/product');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('> Mongo connection established successfully'))
@@ -20,9 +21,20 @@ app.get('/products', async (req, res) => {
     res.render('products', { products });
 });
 
+app.get('/products/new', (req, res) => {
+    res.render('new');
+});
+
 app.get('/products/:id', async (req, res) => {
     const product = await Product.findById(req.params.id);
     res.render('product', { product });
 });
+
+app.post('/products', async (req, res) => {
+    const { name, price, category } = req?.body;
+    const product = new Product({ name, price, category });
+    const response = await product.save();
+    res.redirect(`/products/${response?.id}`);
+})
 
 app.listen(3000, () => console.log('> Server started on port 3000'));
