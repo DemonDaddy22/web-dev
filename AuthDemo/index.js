5f481e35d26167f2d6cdd8253a2405c1fb5f0cee
Rohan Gupta <guptarohan369@gmail.com>
2021-04-17 20:15:30 +0530

    Add regsiter user logic

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,10 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const User = require('./models/user');
 
-const hashPassword = async (password) => {
-    const salt = await bcrypt.genSalt(12);
-    const hash = await bcrypt.hash(password, salt);
-    console.log(salt);
-    console.log(hash);
-}
+mongoose
+    .connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('> Connection established'))
+    .catch((err) => console.log(err));
 
-hashPassword('monkey');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('HOME PAGE');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({ username, password: hash });
+    await user.save();
+    res.redirect('/');
+});
+
+app.get('/secret', (req, res) => {
+    res.send('Cannot access this unless you are authenticated!');
+});
+
+app.listen(3030, () => console.log('> Serving on PORT 3030...'));
---
+++ AuthDemo/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
