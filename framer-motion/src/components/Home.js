816565d65cfa85639be593e613e44a93ecf2201c
Rohan Gupta <guptarohan369@gmail.com>
2021-04-23 20:16:38 +0530

    Add starter code for framer motion demo

---
+++ framer-motion/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const Home = () => {
+    return (
+        <motion.div className='home container' animate={{}}>
+            <h2>Welcome to Pizza Joint</h2>
+            <Link to='/base'>
+                <motion.button animate={{}}>Create Your Pizza</motion.button>
+            </Link>
+        </motion.div>
+    );
+};
+
+export default Home;
