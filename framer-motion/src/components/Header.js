2cbb5cf515c730c078be58049fbdb0cd18f2c213
Rohan Gupta <guptarohan369@gmail.com>
2021-04-24 16:48:00 +0530

    Play with drag properties

--- framer-motion/src/components/Header.js
+++ framer-motion/src/components/Header.js
@@ -14,27 +14,34 @@ const svgVariants = {
 const pathVariants = {
     hidden: {
         opacity: 0,
-        pathLength: 0
+        pathLength: 0,
     },
     visible: {
         opacity: 1,
         pathLength: 1,
         transition: {
             duration: 2,
-            ease: 'easeInOut'
-        }
-    }
-}
+            ease: 'easeInOut',
+        },
+    },
+};
 
 const Header = () => {
     return (
         <header>
-            <div className='logo'>
-                <motion.svg className='pizza-svg' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' variants={svgVariants} initial='hidden' animate='visible'>
+            <motion.div className='logo' drag dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }} dragElastic={0.8}>
+                <motion.svg
+                    className='pizza-svg'
+                    xmlns='http://www.w3.org/2000/svg'
+                    viewBox='0 0 100 100'
+                    variants={svgVariants}
+                    initial='hidden'
+                    animate='visible'
+                >
                     <motion.path fill='none' d='M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z' variants={pathVariants} />
                     <motion.path fill='none' d='M50 30 L50 -10 C50 -10 90 -10 90 30 Z' variants={pathVariants} />
                 </motion.svg>
-            </div>
+            </motion.div>
             <div className='title'>
                 <motion.h1 transition={{ duration: 0.5, type: 'spring', stiffness: 120 }} initial={{ y: -150 }} animate={{ y: -10 }}>
                     Pizza Joint
