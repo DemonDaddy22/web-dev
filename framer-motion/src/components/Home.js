817f305621d9390624be309017aecbc03201bb66
Rohan Gupta <guptarohan369@gmail.com>
2021-04-24 12:58:44 +0530

    Play with keyframes

--- framer-motion/src/components/Home.js
+++ framer-motion/src/components/Home.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const buttonVariants = {
+    visible: {
+        x: [0, -20, 20, -10, 10, 0],
+        transition: {
+            delay: 2,
+            duration: 0.5
+        },
+    },
+    hover: {
+        scale: 1.1,
+        textShadow: '0 0 4px rgba(255, 255, 255, 0.75)',
+        boxShadow: '0 0 8px rgba(255, 255, 255, 0.75)',
+        transition: {
+            duration: 0.2,
+        },
+    },
+};
+
 const Home = () => {
     return (
         <motion.div className='home container' transition={{ delay: 1 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
             <h2>Welcome to Pizza Joint</h2>
             <Link to='/base'>
-                <motion.button
-                    transition={{ duration: 0.2 }}
-                    whileHover={{ scale: 1.1, textShadow: '0 0 4px rgba(255, 255, 255, 0.75)', boxShadow: '0 0 8px rgba(255, 255, 255, 0.75)' }}
-                >
+                <motion.button variants={buttonVariants} animate='visible' whileHover='hover'>
                     Create Your Pizza
                 </motion.button>
             </Link>
