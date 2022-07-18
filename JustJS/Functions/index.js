ada84b42d767f4d25fa223c123c8646a4c5acf5f
Rohan Gupta <guptarohan369@gmail.com>
2022-07-18 23:45:54 +0530

    Implement debounce, throttle and sampling functions

--- JustJS/Functions/index.js
+++ JustJS/Functions/index.js
@@ -74,9 +74,53 @@ const multiply = (a) => (b) => a * b;
  */
 const curryFunc = (fn) => {
     return function curry (...args) {
-        if (fn.length <= args.length) return fn.apply(this, args);
+        if (fn.length <= args.length) return fn.apply(this, args); // base condition
         else return function (...newArgs) {
             return curry.apply(this, args.concat(newArgs));
         }
+    };
+};
+
+/**
+ * 7. Implement a debounce function
+ */
+function debounce (fn, delay, ctx) {
+    let timer;
+
+    return function (...args) {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => fn.apply(this ?? ctx, args), delay);
+    }
+};
+
+/**
+ * 8. Implement a throttle function
+ */
+function throttle (fn, delay, ctx) {
+    let isFunctionCalled = false;
+
+    return function (...args) {
+        if (!isFunctionCalled) {
+            isFunctionCalled = true;
+            fn.apply(this ?? ctx, args);
+            setInterval(() => {
+                isFunctionCalled = false;
+            }, delay);
+        }
+    };
+};
+
+/**
+ * 9. Implement a sampling function
+ */
+function sampling (fn, count, ctx) {
+    let counter = 0;
+
+    return function (...args) {
+        counter += 1;
+        if (counter < count) return;
+
+        fn.apply(this ?? ctx, args);
+        counter = 0;
     };
 };
